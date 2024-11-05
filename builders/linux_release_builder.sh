707d1b01c6dc0204c9bd698a94006c424869f001
azomDev <azom.developer@gmail.com>
2024-11-05 12:25:17 -0500

    fixed linux_release_builder script

--- builders/linux_release_builder.sh
+++ builders/linux_release_builder.sh
@@ -1,4 +1,3 @@
 cargo build --release
-cp target/release/pen files/unix/linux/core
-cd files/unix/linux/
-sha256sum core > core.sha256
+cp target/release/pen release_files/unix/linux/core
+sha256sum release_files/unix/linux/core > release_files/unix/linux/core.sha256
