8a3ee03bfcf8b2e525344a5c39dd1295f74e19ee
azomDev <azom.developer@gmail.com>
2024-11-06 12:34:37 -0500

    added multiple unit test for the same function for refference

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,11 +2,18 @@ use home;
 use pen::utils;
 
 #[test]
-fn get_version_path() {
-    let home_dir = home::home_dir().expect("Failed to get home directory"); // so it works for anyone testing, this is specific to get_version_path
-    let expected_path = home_dir.join(".pen/python_versions/python_3.11.9");
-    let version_path = utils::get_version_path("3.11.9");
-    assert_eq!(version_path, expected_path);
+fn get_version_paths() {
+    let home_dir = home::home_dir().expect("Failed to get home directory");
+
+    // A list of versions to test
+    let versions = vec!["3.11.9", "3.10.8", "3.9.7", "3.8.10", "3.7.9"];
+
+    // Loop through the versions and test each one
+    for version in versions {
+        let expected_path = home_dir.join(format!(".pen/python_versions/python_{}", version));
+        let version_path = utils::get_version_path(version);
+        assert_eq!(version_path, expected_path, "Version mismatch for {}", version);
+    }
 }
 
 
